f621c7ff6b627a813b6f9d2d41d3270cab755922
shaurya5 <gargshaurya28@gmail.com>
2022-09-25 17:35:59 +0530

    q2 done

---
+++ Assi-1/q2.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sort(int arr[], int size) {
+  for (int j = 1; j < size; j++) {
+    int key = arr[j];
+    int i = j - 1;
+    while (i > -1 && arr[i] > key) {
+      arr[i + 1] = arr[i];
+      i = i - 1;
+    }
+    arr[i + 1] = key;
+  }
+}
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  int arr[n];
+  for(int i=0; i<n; i++) {
+    cin >> arr[i];  
+  }
+  sort(arr, n);
+
+  int ans = 0;
+  int sum = 0;
+  for(int i : arr) {
+    sum += i;
+    if(sum > k) break;
+    ans++;
+  }
+  cout << ans << endl;
+  
+  return 0;
+}
