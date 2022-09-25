126b20c691dc1d66ecf35e5e905a71d3c6d4493c
shaurya5 <gargshaurya28@gmail.com>
2022-09-25 12:41:18 +0530

    initial commit

---
+++ Assi-1/q1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+struct Node {
+	int data;
+	Node *next;
+};
+
+
+int main() {
+	
+	return 0;
+}
---
+++ Assi-1/q4.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+struct Node {
+	int data;
+	Node *next;
+};
+
+void createLL() {
+  
+}
+
+int main() {
+	
+	return 0;
+}
