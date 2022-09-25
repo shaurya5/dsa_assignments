7e69c25ccec0fc3bfa40954407fb6b6f1ac55f02
shaurya5 <gargshaurya28@gmail.com>
2022-09-25 22:01:41 +0530

    q5 done

--- Assi-1/q5.cpp
+++ Assi-1/q5.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 
 int main() {
+  int n;
+  cin >> n;
+  int arr[n];
+  for(int i=0; i<n; i++) {
+    cin >> arr[i];
+  }
+
+  int maxId;
+  int count = 0;
   
+  for(int i=0; i<n-1; i++) {
+    maxId = i;
+    for(int j=i+1; j<n; j++) {
+      if(arr[j] > arr[maxId]) {
+        maxId = j;
+      }
+
+    } 
+    if(maxId != i) {
+      swap(arr[i], arr[maxId]);
+      count++;
+    }
+  }
+
+  cout << count;
   return 0;
 }
