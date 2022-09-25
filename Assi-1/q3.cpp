03fabac195345e4dfb3ccdbd5591518ed1c7230e
shaurya5 <gargshaurya28@gmail.com>
2022-09-25 15:35:47 +0530

    q3 done

---
+++ Assi-1/q3.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void recurse(int size, int open, int close, string op, vector<string> &ans) {
+  if(op.size() == size * 2) {
+    ans.push_back(op);
+    return;
+  }
+
+  if(open < size) 
+    recurse(size, open+1, close, op+"(", ans);
+
+  if(close < open)
+    recurse(size, open, close+1, op+")", ans);
+}
+
+int main() {
+  cout << "Enter input size: ";
+  int size;
+  cin >> size;
+
+  int open = 0, close = 0;
+  string s = "";
+  vector<string> ans;
+
+  recurse(size, open, close, s, ans);
+
+  for(auto str : ans) {
+    cout << str << endl;
+  }
+
+  return 0;
+}
--- Assi-1/q4.cpp
+++ Assi-1/q4.cpp
@@ -1,15 +1,43 @@
 #include <bits/stdc++.h>
+using namespace std;
 
 struct Node {
 	int data;
 	Node *next;
+
+	Node(int val) {
+		next = NULL;
+		data = val;
+	}
 };
 
-void createLL() {
-  
+Node* createLL(int size) {
+	vector<int> nos;
+	while(size--) {
+		cout << "Enter an integer: ";
+		int inp;
+		cin >> inp;
+		nos.push_back(inp);
+	}
+
+	
 }
 
 int main() {
-	
+	cout << "Enter size: ";
+	int size;
+	cin >> size;
+
+	Node *head = createLL(size);
+	Node *fast = head;
+	Node *slow = head;
+
+	while(fast != slow) {
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+
+	cout << fast->data;
+
 	return 0;
 }
