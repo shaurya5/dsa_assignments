af51176739b15867a5abc1825c16c06165f0145e
shaurya5 <gargshaurya28@gmail.com>
2022-10-10 16:27:21 +0530

    q1 done

---
+++ Assi-2/q1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+bool solve(int *arr, int left, int right, int target) {
+	if(left == right) return false;
+	if(arr[left] + arr[right] == target) {
+		return true;
+	}
+	if(arr[left] + arr[right] >= target) {
+		return solve(arr, left, right-1, target);
+	}
+	return solve(arr, left+1, right, target);
+}
+
+int main() {
+	int arr[] = {1,2,3,4,5,6,7,8,9};
+	cout << solve(arr, 0, 8, 10);
+  	return 0;
+}
