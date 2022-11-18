eb55a180a4ad1f7a6e7245d115a024d01727a687
shaurya5 <gargshaurya28@gmail.com>
2022-11-18 19:53:21 +0530

    random

--- Assi-3/q4.cpp
+++ Assi-3/q4.cpp
@@ -8,6 +8,7 @@ int main() {
 	cin >> n;
 	unordered_map<string, vector<pair<int,int>>> m; // name: [{index, value}]
 
+    // creating an unordered map with name, {index, value}
 	for(int i = 0; i < n; i++) {
 		string s;
 		int x;
@@ -15,8 +16,9 @@ int main() {
 		m[s].push_back({i,x});
 	}
 
+    // finding max score
 	int maxScore = -1;
-	for(auto it : m) {
+	for(auto it : m) { 
 		int sum = 0;
 		for(auto p : it.second) {
 			sum += p.second;
@@ -34,15 +36,18 @@ int main() {
 	// 	}
 	// 	cout << endl;
 	// }
-    
+
 	vector<pair<int,string>> v;
 	int minIndex = INT_MAX;
 
+    // finding mini index of max score
 	for(auto it : m) {
 		int sum = 0;
 		for(auto p : it.second) {
 			sum += p.second;
-			if(sum >= maxScore && it.second[it.second.size() - 1].second >= maxScore) {
+            int vSize = it.second.size();
+            int lastValue = it.second[vSize - 1].second;
+			if(sum >= maxScore && lastValue >= maxScore) {
 				minIndex = min(minIndex, p.first);
 				v.push_back({p.first, it.first});  // p.first -> index , it.first -> name
 				break;
@@ -50,6 +55,7 @@ int main() {
 		}
 	}
 
+    // printing name with max score and least index
 	for(auto i : v) {
 		if(i.first == minIndex) {
 			cout << i.second << endl;
